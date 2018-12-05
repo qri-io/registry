ca61bbcc94471d1a03384b2b9aa3c557d6d4bedd
b5 <sparkle_pony_2000@qri.io>
2018-12-05 15:22:46 -0500

    feat(DsyncFetch): add dsync dataset fetch method to registry client

--- regclient/dsync.go
+++ regclient/dsync.go
@@ -8,6 +8,7 @@ import (
 	"github.com/qri-io/dag/dsync"
 
 	ipld "gx/ipfs/QmR7TcHkR9nxkUorfi8XMTAMLUK7GiP64TWWBzY3aacc1o/go-ipld-format"
+	coreiface "gx/ipfs/QmUJYo4etAQqFfSS2rarFAE97eNGB8ej64YkRT2SmsYD4r/go-ipfs/core/coreapi/interface"
 )
 
 // DsyncSend pushes a DAG to the registry
@@ -22,4 +23,18 @@ func (c *Client) DsyncSend(ctx context.Context, ng ipld.NodeGetter, mfst *dag.Ma
 	}
 
 	return snd.Do()
+}
+
+// DsyncFetch fetches an entire DAG designated by root path
+func (c *Client) DsyncFetch(ctx context.Context, path string, ng ipld.NodeGetter, bapi coreiface.BlockAPI) error {
+	remote := &dsync.HTTPRemote{
+		URL: fmt.Sprintf("%s/dsync", c.cfg.Location),
+	}
+
+	fetch, err := dsync.NewFetch(ctx, path, ng, bapi, remote)
+	if err != nil {
+		return err
+	}
+
+	return fetch.Do()
 }
